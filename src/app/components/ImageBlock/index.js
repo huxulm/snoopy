4df35d92004675def4357b049acdf00d04b3e3e4
jackdon <janeysesions@gmail.com>
2019-01-30 20:56:19 +0800

    change image block container with React.Fragment

--- src/app/components/ImageBlock/index.js
+++ src/app/components/ImageBlock/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ImageZoom from 'react-medium-image-zoom';
 import styled from "styled-components";
 
-const Container = styled.div``;
-
 class ImageBlock extends React.Component {
   constructor(props) {
     super(props);
@@ -11,20 +9,22 @@ class ImageBlock extends React.Component {
 
   render() {
     const { src = "", alt = "No Description", className } = this.props;
-    return (<Container>
-      <ImageZoom
-        image={{
-          src,
-          alt,
-          className,
-          style: { width: '50em' }
-        }}
-        zoomImage={{
-          src,
-          alt,
-        }}
-      />
-    </Container>)
+    return (
+      <React.Fragment>
+        <ImageZoom
+          image={{
+            src,
+            alt,
+            className,
+            style: { width: "50em" }
+          }}
+          zoomImage={{
+            src,
+            alt
+          }}
+        />
+      </React.Fragment>
+    );
   }
 }
 
