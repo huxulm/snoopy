475d7c253bd4b0883f0eb67ce76348264c811084
jackdon <janeysesions@gmail.com>
2019-01-31 22:23:31 +0800

    add footer link style

--- src/app/pages/Footer/index.js
+++ src/app/pages/Footer/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Container, CopyRight } from "./elements";
-
+const style = {
+  color: "#2C2C2C",
+  fontWeight: 500,
+  verticalAlign: "text-top",
+  textAlign: "center"
+};
 class Footer extends React.Component {
   constructor(props) {
     super(props);
@@ -10,7 +15,15 @@ class Footer extends React.Component {
     return (
       <Container {...this.props}>
         <CopyRight>
-          ©2017~2019 Snoopy. 皖ICP备17008795号<br/> Powered by R&N.
+          ©2017~2019{" "}
+          <a href="http://www.xulingming.cn" style={style}>
+            Snoopy
+          </a>
+          . 皖ICP备
+          <a style={style} href="#">
+            17008795
+          </a>
+          号<br /> Powered by R&N.
         </CopyRight>
       </Container>
     );
