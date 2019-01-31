a6934260166712ac06298a7ec09421ac8f52f21a
jackdon <janeysesions@gmail.com>
2019-01-31 23:17:29 +0800

    local image

--- src/app/components/SlideImage/index.js
+++ src/app/components/SlideImage/index.js
@@ -13,27 +13,27 @@ class SlideImage extends React.Component {
   state = {
     imgs: [
       {
-        url: 'https://snoopy-blog.oss-cn-shanghai.aliyuncs.com/space_imgs/space_imgs/03may15eclipse_ouellet_full.jpg',
+        url: 'http://snp-assets.oss-cn-shanghai.aliyuncs.com/blog_covers/home_page_1.jpg',
         delay: '0s',
       },
       {
-        url: 'https://snoopy-blog.oss-cn-shanghai.aliyuncs.com/space_imgs/space_imgs/040523cruxa_seip_full.jpg',
+        url: 'http://snp-assets.oss-cn-shanghai.aliyuncs.com/blog_covers/home_page_2.jpg',
         delay: '6s',
       },
       {
-        url: 'https://24.media.tumblr.com/f87b54bbce49e59debf7606662f54862/tumblr_n0hpxxDOJ91st5lhmo1_1280.jpg',
+        url: 'http://snp-assets.oss-cn-shanghai.aliyuncs.com/blog_covers/home_page_3.jpg',
         delay: '12s',
       },
       {
-        url: 'https://24.media.tumblr.com/0df0b55a4615378cf593241bad80a7da/tumblr_n0hpwpZrVc1st5lhmo1_1280.jpg',
+        url: 'http://snp-assets.oss-cn-shanghai.aliyuncs.com/blog_covers/home_page_4.jpg',
         delay: '18s',
       },
       {
-        url: 'https://24.media.tumblr.com/b94dbb2a392198d5cc39c19959598229/tumblr_n0hpthN8VH1st5lhmo1_1280.jpg',
+        url: 'https://snoopy-blog.oss-cn-shanghai.aliyuncs.com/space_imgs/space_imgs/03may15eclipse_ouellet_full.jpg',
         delay: '24s',
       },
       {
-        url: 'https://31.media.tumblr.com/67d222ee577fc35faca83f0e08efc48e/tumblr_mzzqt7wyEU1st5lhmo1_1280.jpg',
+        url: 'https://snoopy-blog.oss-cn-shanghai.aliyuncs.com/space_imgs/space_imgs/040523cruxa_seip_full.jpg',
         delay: '30s',
       },
     ]
