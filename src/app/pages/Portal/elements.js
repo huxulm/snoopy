6f04fd21f8139c74b48d6390f17c712054f81936
jackdon <janeysesions@gmail.com>
2019-02-03 10:17:54 +0800

    change home page

--- src/app/pages/Portal/elements.js
+++ src/app/pages/Portal/elements.js
@@ -5,6 +5,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  height: 100%;
 `;
 
 export const PictureContainer = styled(Centered)`
--- src/app/pages/Portal/index.js
+++ src/app/pages/Portal/index.js
@@ -6,6 +6,7 @@ import Footer from "../Footer/index";
 
 import _img from "./2013-11_C2P_2048px.jpg";
 import Christmas from "./christmas";
+import NewYear from "./NewYear";
 
 const WIDTH = 320;
 const HEIGHT = 320;
@@ -17,66 +18,66 @@ class Portal extends Component {
 
   componentDidMount() {
     this.props.signals.loadPortalPage();
-    this.ctx = this.canvasRef.getContext("2d");
-    this.canvasRef.width = 320;
-    this.canvasRef.height = 320;
-    var canvas = this.canvasRef;
-    var ctx = this.canvasRef.getContext("2d");
-    var WIDTH = 320;
-    var HEIGHT = 320;
-    this.canvasRef.width = WIDTH;
-    this.canvasRef.height = HEIGHT;
-    clearCanvas();
+    // this.ctx = this.canvasRef.getContext("2d");
+    // this.canvasRef.width = 320;
+    // this.canvasRef.height = 320;
+    // var canvas = this.canvasRef;
+    // var ctx = this.canvasRef.getContext("2d");
+    // var WIDTH = 320;
+    // var HEIGHT = 320;
+    // this.canvasRef.width = WIDTH;
+    // this.canvasRef.height = HEIGHT;
+    // clearCanvas();
 
-    var particles = [];
-    for (var i = 0; i < WIDTH; i++) {
-      particles.push({
-        x: Math.random() * WIDTH,
-        y: Math.random() * HEIGHT,
-        r: Math.random() * 2 + 1
-      })
-    }
+    // var particles = [];
+    // for (var i = 0; i < WIDTH; i++) {
+    //   particles.push({
+    //     x: Math.random() * WIDTH,
+    //     y: Math.random() * HEIGHT,
+    //     r: Math.random() * 2 + 1
+    //   })
+    // }
 
-    function draw() {
-      clearCanvas();
-      ctx.fillStyle = "rgba(255, 255, 255, 0.6)";
-      ctx.beginPath();
+    // function draw() {
+    //   clearCanvas();
+    //   ctx.fillStyle = "rgba(255, 255, 255, 0.6)";
+    //   ctx.beginPath();
 
-      for (let i = 0; i < WIDTH; i++) {
-        let p = particles[i];
-        ctx.moveTo(p.x, p.y);
-        ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2, true);
-      }
-      ctx.fill();
-      update();
-    }
+    //   for (let i = 0; i < WIDTH; i++) {
+    //     let p = particles[i];
+    //     ctx.moveTo(p.x, p.y);
+    //     ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2, true);
+    //   }
+    //   ctx.fill();
+    //   update();
+    // }
 
-    function update() {
-      for (let i = 0; i < WIDTH; i++) {
-        let p = particles[i];
-        p.y += p.r;
-        if (p.y > canvas.height) {
-          particles[i] = {
-            x: Math.random() * canvas.width,
-            y: -10,
-            r: p.r
-          };
-        }
-      }
-    }
+    // function update() {
+    //   for (let i = 0; i < WIDTH; i++) {
+    //     let p = particles[i];
+    //     p.y += p.r;
+    //     if (p.y > canvas.height) {
+    //       particles[i] = {
+    //         x: Math.random() * canvas.width,
+    //         y: -10,
+    //         r: p.r
+    //       };
+    //     }
+    //   }
+    // }
 
-    var timer = setInterval(draw, 50);
+    // var timer = setInterval(draw, 50);
 
-    function clearCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
+    // function clearCanvas() {
+    //   ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // }
   }
 
   render() {
     const { signals } = this.props;
     return (
       <Container>
-        <Header />
+        {/* <Header />
         <Content>
           <CanvasContainer>
             <canvas ref={e => (this.canvasRef = e)} style={{
@@ -89,7 +90,8 @@ class Portal extends Component {
             }}/>
           </CanvasContainer>
           <Christmas/>
-        </Content>
+        </Content> */}
+        <NewYear />
       </Container>
     );
   }
